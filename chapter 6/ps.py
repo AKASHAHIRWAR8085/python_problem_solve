24abf470673690a7baa667389dd23ba99acc5710
akash ahirwar <Aakash@LAPTOP-BPLV7BJ6>
2026-06-26 22:43:17 +0530

    project updated

--- chapter 6/ps.py
+++ chapter 6/ps.py
@@ -17,9 +17,9 @@
 
 # qustion no 2:- wap to find the whether a student has passed to faild ith requies a total of 40% and at least 33% in each subject to pass assume 3 subjects and take marks as an input from the user 
    
-marks1 = int(input("enter marks of subject 1:"))
-marks2 = int(input("enter marks of subject 2:"))
-marks3 = int(input("enter marks of subject 3:"))
+# marks1 = int(input("enter marks of subject 1:"))
+# marks2 = int(input("enter marks of subject 2:"))
+# marks3 = int(input("enter marks of subject 3:"))
 
 #  check for total percentage
 # total_percentage = (marks1 + marks2 + marks3) / 3
@@ -28,3 +28,31 @@ marks3 = int(input("enter marks of subject 3:"))
 #     print("the student has passed")
 # else:
 #     print("the student has failed")
+
+# Qus No:-3  wap tp fimd whether a given username contains less then 10 characters these spams ?
+
+# username = input("enter your username:")
+# if(len(username) < 10):
+#     print(" your username contion less then 10 characters")
+# else:
+#     print("all is well your username is valid")
+
+
+# Qus No:-4 wap which finds out whether a gaiven name is present in a list or not 
+
+# names = ["Alice", "Bob", "Charlie", "David"]
+# name = input("enter a name:")
+
+# if name in names:
+#     print(f"{name} is present in the list.")
+# else:
+#     print(f"{name} is not present in the list.")
+
+# Qus No:-5 wap to find out whether a given post taking about "aakash"or not
+
+# post = input("enter your post:")
+# if ("aakash" in post):
+#     print("The post is talking about Aakash.")
+
+# else:
+#     print("The post is not talking about Aakash.")    
